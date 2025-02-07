b83a3212ef56fde55d38993b7ce123b890619a73
abhilash <abhigxtheupm@gmail.com>
2025-02-07 22:59:08 +0530

    commit 4

--- WealthWise-main/src/components/Home.js
+++ WealthWise-main/src/components/Home.js
@@ -203,7 +203,7 @@ const Home = ({ mail }) => {
           ];
 
       
-        // investing here for now 
+        // investing here for now  stocks 
         const investments = [
         {
             type: 'Stocks',
